d6659511e6d050ae9918a2b886770dc9eed90edf
nikhilepparla <e.nikhil1999@gmail.com>
2023-08-01 06:22:03 +0530

    call by value

--- Functionsin.c
+++ Functionsin.c
@@ -1,16 +1,12 @@
 #include <stdio.h>
-void f(int *, int *);
+void f(int, int);
 int main()
 {
   int a = 20, b = 30;
-  // a++;
-  f(&a, &b);
   printf("a = %d b = %d", a, b);
 }
-void f(int *x, int *y)
+void f(int x, int y)
 {
-  int temp = *x;
-  temp++;
-  temp++;
-  *x = temp;
+  x++;
+  y++;
 }

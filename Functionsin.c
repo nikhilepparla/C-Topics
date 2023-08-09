c20d1db8dcc15bd569a18c329d3ad69a38245ec3
nikhilepparla <e.nikhil1999@gmail.com>
2023-08-09 10:29:42 +0530

    Pattern 1 -Increment

--- Functionsin.c
+++ Functionsin.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
-void f(int, int);
+
 int main()
 {
-  int a = 20, b = 30;
-  printf("a = %d b = %d", a, b);
-}
-void f(int x, int y)
-{
-  x++;
-  y++;
+  for (int i = 0; i < 4; i++)
+  {
+    for (int j = 0; j <= i; j++)
+    {
+      printf("*");
+    }
+    printf("\n");
+  }
 }

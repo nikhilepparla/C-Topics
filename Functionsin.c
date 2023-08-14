6492c72d04fe8931884f85557bf68b7d88b41941
nikhilepparla <e.nikhil1999@gmail.com>
2023-08-14 10:26:17 +0530

    goto in loops

--- Functionsin.c
+++ Functionsin.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-// find the nth value formulae is
-// an = a1 +(n-1)*d
 int main()
 {
-  float hour, min;
-  min = 1 * 60.0;
-  hour = 60 * min;
-  unsigned input;
-  scanf("%u", &input);
-  printf("Milutes is %f", input / min);
-  printf("Hours is %f", input / hour);
+  int a = 9;
+  if (a == 9)
+    goto label1;
+  printf("no jump");
+
+label1:
+  printf("number is 9");
 }

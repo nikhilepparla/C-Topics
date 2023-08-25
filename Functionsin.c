ee4d3677603cbdc99dbf5ba7e5245a7fb6f827c9
nikhilepparla <e.nikhil1999@gmail.com>
2023-08-25 07:29:11 +0530

    Assigning value to variable

--- Functionsin.c
+++ Functionsin.c
@@ -1,13 +1,8 @@
 #include <stdio.h>
-struct NewName
-{
-  char b;
-  char a[10];
-};
-
 int main()
 {
-  struct NewName x1;
-  x1 = {'a'};
-  printf("%s", x1.a[0]);
+  int x = 10, y = 20;
+  int z = (x++, x++);
+  printf("%d", z);
 }
+//output is 11

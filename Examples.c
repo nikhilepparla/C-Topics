bde6fda2073a3a30c90ff44cda4d4d391b7907db
nikhilepparla <e.nikhil1999@gmail.com>
2023-08-29 06:28:49 +0530

    Funtions with exapmle

--- Examples.c
+++ Examples.c
@@ -1,20 +1,47 @@
 #include <stdio.h>
 
+int f1(void);
+
+int f2(void);
+
+int f3(void);
+
+int x = 10;
+
 int main()
+
 {
-  int arr[] = {10, 11, 12, 190, 89};
-  int min, max;
-  min = max = arr[0];
-  for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); i++)
-  {
-    if (arr[i] > max)
-    {
-      max = arr[i];
-    }
-    else if (min > arr[i])
-    {
-      min = arr[i];
-    }
-  }
-  printf("MAX is %d MIN is %d", max, min);
+  int x = 1;
+
+  x += f1() + f2() + f3() + f2();
+
+  printf("\n%d", x);
+
+  return 0;
 }
+
+int f1()
+{
+  int x = 25;
+  x++;
+  printf("f1 %d", x);
+  return x;
+}
+
+int f2()
+{
+  static int x = 50;
+  x++;
+  printf("f2 %d", x);
+
+  return x;
+}
+
+int f3()
+{
+  x *= 10;
+  printf("f3 %d", x);
+  
+  return x;
+}
+//O/P = 230

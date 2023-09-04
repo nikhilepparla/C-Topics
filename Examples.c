869f76e4122fb53af88eed6a3ebc4828a71832f2
nikhilepparla <e.nikhil1999@gmail.com>
2023-09-04 07:50:36 +0530

    Pointers in c

--- Examples.c
+++ Examples.c
@@ -1,47 +1,9 @@
 #include <stdio.h>
-
-int f1(void);
-
-int f2(void);
-
-int f3(void);
-
-int x = 10;
-
 int main()
-
 {
-  int x = 1;
-
-  x += f1() + f2() + f3() + f2();
-
-  printf("\n%d", x);
-
-  return 0;
+  static int a[] = {10, 20, 30, 40, 50};
+  // This will subtract 40-30
+  printf("%d", a[3] - a[2]);
+  // compiler will do the operation like this
+  printf("%d", *(a + 3) - *(a + 2));
 }
-
-int f1()
-{
-  int x = 25;
-  x++;
-  printf("f1 %d", x);
-  return x;
-}
-
-int f2()
-{
-  static int x = 50;
-  x++;
-  printf("f2 %d", x);
-
-  return x;
-}
-
-int f3()
-{
-  x *= 10;
-  printf("f3 %d", x);
-  
-  return x;
-}
-//O/P = 230
